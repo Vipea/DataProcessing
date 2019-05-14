ac2e6a2c91958672e5cf8a9a19c942323a8c2e45
Vipea <fringsmax@gmail.com>
2019-05-14 14:10:58 +0200

    added hover on circles, accounted for circle surface

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -52,12 +52,7 @@ Promise.all(requests).then(function(response) {
 
     years = {}
 
-    new_dataset = {}
-    Object.keys(dataset["GDP"]).forEach(function(country) {
-      if (dataset["teenViolence"].hasOwnProperty(country) && dataset["teenPregnancy"].hasOwnProperty(country)) {
-        new_dataset[country] = {}
-      }
-    })
+
 
     // For each year
     for (year = start_year; year <= end_year; year++) {
@@ -67,32 +62,28 @@ Promise.all(requests).then(function(response) {
       // For each country
       Object.keys(dataset["GDP"]).forEach(function(country) {
         if (dataset["teenViolence"].hasOwnProperty(country) && dataset["teenPregnancy"].hasOwnProperty(country)) {
+          new_dataset = {}
+          new_dataset["country"] = country
 
           // For each dataset
           for (i=0; i < datasets.length; i++) {
 
             dataset[datasets[i]][country].forEach(function(this_year) {
             if (year == this_year.Year) {
-              console.log("INCLUDE THIS " + datasets[i] + " VALUE OF " + country + " IN THE YEAR " + this_year.Year)
-              new_dataset[country][datasets[i]] = this_year.Datapoint
-              console.log(this_year.Datapoint)
+              new_dataset[datasets[i]] = this_year.Datapoint
               return
             }
-            else if (scatter_years.includes(parseInt(this_year.Time))) {
-              console.log("INCLUDE THIS " + datasets[i] + " VALUE OF " + country + " IN THE YEAR " + this_year.Time)
-              new_dataset[country][datasets[i]] = this_year.Datapoint
-              console.log(this_year.Datapoint)
+            else if (year == this_year.Time) {
+              new_dataset[datasets[i]] = this_year.Datapoint
               return
             }
           })
           } // closes for each dataset
+          years[year].push(new_dataset)
         } // closes common countries
     }) // closes countries for each
-    years[year].push(new_dataset)
-  } // closes for each year
-  console.log(years)
 
-    exit()
+  } // closes for each year
 
 
 
@@ -100,63 +91,139 @@ Promise.all(requests).then(function(response) {
 
     // ??? missing countries or values ?
 
-    countries_in_common = []
 
-    Object.keys(dataset["GDP"]).forEach(function(country) {
-      if (dataset["teenViolence"].hasOwnProperty(country) && dataset["teenPregnancy"].hasOwnProperty(country)) {
-        countries_in_common.push(country)
-        new_dataset[country] = years
-
-
-        // Put all values in their place within the new_dataset
-        for (i=0; i < datasets.length; i++) {
-
-          // tester = 0
-
-            dataset[datasets[i]][country].forEach(function(this_year) {
-              // tester++
-              // if (tester == 30) {
-              //   exit()
-              // }
-              if (scatter_years.includes(parseInt(this_year.Year))) {
-                console.log("INCLUDE THIS " + datasets[i] + " VALUE OF " + country + " IN THE YEAR " + this_year.Year)
-                new_dataset[country][this_year.Year][datasets[i]] = this_year.Datapoint
-                console.log(this_year.Datapoint)
-              }
-              else if (scatter_years.includes(parseInt(this_year.Time))) {
-                console.log("INCLUDE THIS " + datasets[i] + " VALUE OF " + country + " IN THE YEAR " + this_year.Time)
-                new_dataset[country][this_year.Time][datasets[i]] = this_year.Datapoint
-                console.log(this_year.Datapoint)
-              }
-            })
-        }
-      }
-      // {YEAR: [ COUNTRY: {}
-        //
-    }) // end for each country
-    console.log(new_dataset)
-    console.log([dataset.GDP])
-    console.log([dataset.teenViolence])
 
     // console.log(GDP2016)
+    var scatter_this_year = 2012
 
-    //Create SVG element
+    const margin = {top: 30, right: 50, bottom: 50, left: 50};
+    const innerWidth = w - margin.right - margin.left;
+    const innerHeight = h - margin.top - margin.bottom;
+
+    const yScale = d3.scaleLinear()
+     .domain([0, 30]) // !!! don't hardcode
+     .range([innerHeight, 0]);
+
+
+   const xScale = d3.scaleLinear()
+    .domain([0, 30]) // !!! don't hardcode
+    .range([0, innerWidth]);
+
+    // Create div to show value on hover
+    const div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+    // Create SVG element
     var svg = d3.select("body")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
 
-    svg.selectAll("circle")
-       .data(Object.keys(dataset))
+
+
+    const circles = svg.selectAll("circle")
+       .data(years[scatter_this_year])
        .enter()
        .append("circle")
-       // .attr("cx", function(d) {
-       //      return d[0];
-       //  })
-       //  .attr("cy", function(d) {
-       //       return d[1];
-       //  })
-       //  .attr("r", 5);
+       .attr("cx", function(d) {
+            return xScale(d.teenPregnancy);
+        })
+        .attr("cy", function(d) {
+             return yScale(d.teenViolence);
+        })
+
+        // Sets the surface area of the circle in such a way that the circle surface areas correspond with the GDP
+        .attr("r", function(d) {
+            return Math.sqrt(d.GDP) / 30
+          })
+        .attr("fill", "#3399ff");
+
+            // Add hover effect on mouse over
+            circles.on('mouseover',
+            function (d, i) {
+                               d3.select(
+                               this).transition("barOpacity")
+
+                              .attr('opacity', '.5')
+
+                              // Makes div appear on hover
+                              div.transition("divAppear")
+
+                              .style("opacity", 1)
+                              div.html(Math.round(d.GDP))
+                              .style("left", (
+                                    d3.event.pageX + 10) + "px")
+                              .style("top", (
+                                    d3.event.pageY - 15) + "px");
+                            })
+
+            // Disable hover effect on mouse out
+            .on('mouseout', function (d, i) {
+                                              d3.select(this).transition(
+                                                              "barDisappear")
+
+                                              .attr('opacity', '1');
+
+                                              // Make the div disappear
+                                              div.transition("divDisappear")
+
+                                              .style("opacity", 0);
+                                            })
+
+
+        // Set footer with the dataset source
+        d3.select("body")
+          .append("footer")
+          .append("a")
+          .attr("href", "https://www.clo.nl/en/indicators/en0218-ozone-layer")
+          .attr("target", "_blank")
+          .text("Dataset source");
+
+        // Set title
+        svg.append("text")
+            .attr("x", margin.left)
+            .attr("y", margin.top / 2)
+            .attr("text-anchor", "left")
+            .style("font-size", "14px")
+            .style("font-weight", "bold")
+            .text("Scatter plot of teen pregnancy rates and teen violence rates");
+
+        // Append x axis to the SVG
+        const xAxis = svg.append("g")
+          .classed("xAxis", true)
+          .attr('transform', `translate(${margin.left},
+                                        ${innerHeight + margin.top})`)
+          .call(d3.axisBottom(xScale))
+
+          svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("x", w/2)
+        .attr("y", h - 10)
+        .text("Teen pregnancy");
+
+
+        // Append y axis to the SVG
+        const yAxis = svg.append("g")
+          .classed("yAxis", true)
+          .attr('transform', `translate(${margin.left}, ${margin.top})`)
+          .call(d3.axisLeft(yScale));
+
+          svg.append("text")
+              .attr("class", "y label")
+              .attr("text-anchor", "end")
+              .attr("y", 0)
+              .attr("x", - h / 3)
+              .attr("dy", ".75em")
+              .attr("transform", "rotate(-90)")
+              .text("Teen violence");
+
+              // Create select dropdown
+              var svg = d3.select("body")
+              for (i = start_year; i <= end_year; i++) {
+              .append("select");
+            } // ??? kan dit
 
 }).catch(function(e){
     throw(e);
